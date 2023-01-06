5e8c0d437e656297083acc2f7f7b399f4b8bba68
yilmazbaris6363 <yilmazbaris6363@gmail.com>
2023-01-06 12:50:12 +0300

    Add project files.

---
+++ SignalRProject/Hubs/MessageHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRProject.Hubs
+{
+    public class MessageHub:Hub
+    {
+
+      
+
+
+
+    }
+
+    public class MessageHubService
+    {
+        private readonly IHubContext<MessageHub> _hubcontext;
+
+        public MessageHubService(IHubContext<MessageHub> hubcontext)
+        {
+            _hubcontext = hubcontext;
+        }
+
+
+        public async Task SendMessage(string message)
+        {
+            await _hubcontext.Clients.All.SendAsync("SendMessage",message);
+
+
+        }
+    }
+}
